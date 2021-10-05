a2cf9004500eda134ccd7c8fcce8592c8821fe25
GamerNik27 <niklasnjohansen@gmail.com>
2021-10-05 11:50:46 +0200

    Added code to find a random value for movement
    Took a little searching around, but finally found a somewhat good-looking solution for finding a random value in the interval [-s ; s]

--- src/PredatorPray.java
+++ src/PredatorPray.java
@@ -1,4 +1,6 @@
 
+import java.util.Random;
+
 public class PredatorPray {
 
 	public static void main(String[] args) {
@@ -9,6 +11,12 @@ public class PredatorPray {
 	
 	public static void runSimulation(int n, int s, int t) {
 		System.out.println("n=" + n + "s=" + s + "t=" + t);
+		
+		Random rand = new Random();
+		
+		int randPos = rand.nextInt(s+1);
+		int randBool = rand.nextBoolean() ? 1 : 0; //gives 1, if true and 0 if not
+		int randMove = randPos - 2 * randPos * randBool; //finally a pseudo-random value between [-s;s]
 		
 		
 	}
