60f9bee9117477967e86ad5c6fbecbe29ff06b1f
Niklas <niklasnjohansen@gmail.com>
2021-10-06 23:59:39 +0200

    Got carried away while working on Problem 3.a
    Summary:
    
    - Actually calls the function in main now - was wondering why I wasn't getting anything in the console...
    
    - A check is performed, to ensure valid parameters.
    
    - Started out by setting up a randomizer to be used to generate a random starting point for the beasts (prey and predator), and to generate a random distance to move. The starting positions were then printed, to ensure the starting positions *actually were* random (pseudo, but you get what I mean).
    
    - Added a for-loop to generate lots of random positions, based on the amount  of turns.
    
    - Rememberd that the new random positions should also be printed - whoops.
    
    - Moved the randomizer for the mover into a seperate function.
    
    - Implemented the movement for the prey - predator will be implemented tomorrow.
    
    - Added a function for checking collision between the beasts - this function also terminates the program.

--- src/NumberCheck.java
+++ src/NumberCheck.java
@@ -2,34 +2,9 @@
 public class NumberCheck {
 
 	public static void main(String[] args) {
-		//doing a little thinking, to figure out a possible solution:
-		
-		//start a for-loop to split 'number' into digits - but every 2nd digit is used as is
-		//use substring to split 'number' into each "digit" (it's still a string, so they're not digits yet)
-		//use parseInt() or valueOf() to get the value of each "digit" - 
-		
-		//repeat the above section until the entire number is processed 
-		
-		
-		//start computing cross sum:
-		
-		//double the first digit - if >10, then (mod 10) + 1 (fx. 7 becomes ((7*2) mod 10) + 1)
-		//potentially store each new digit in another int[] for debug purposes
-		//add the new digits together
-		//determine (crossSum mod 10 == 0): return boolean value of this
-		
-		//don't know if the string will contain a negative numbers
-		//not sure how those should be handled - should they be treated as invalid inputs?
-		//Or maybe I should just use the absolute value of it
-		//would also need to make sure the length doesn't count the negative sign ._.
-		
-		
-//		String num = "1234";
-//		String firstNum = num.substring(0,1);
-//		int firstInt = Integer.valueOf(firstNum);
-//		System.out.println(firstInt);
 		
 		System.out.println(check("01230"));
+		
 	}
 	
 	public static boolean check(String number) {
--- src/PredatorPray.java
+++ src/PredatorPray.java
@@ -1,24 +1,79 @@
 
 import java.util.Random;
+import java.awt.Point;
 
 public class PredatorPray {
-
+	
 	public static void main(String[] args) {
 		
-		
+		runSimulation(10, 5, 10);
 		
 	}
 	
+	
 	public static void runSimulation(int n, int s, int t) {
-		System.out.println("n=" + n + "s=" + s + "t=" + t);
+		System.out.println("n=" + n + " s=" + s + " t=" + t);
+		if (n <= 0 || s <= 0 || t < 0) {
+			System.out.println("Illegal Parameters!");
+		}
 		
 		Random rand = new Random();
 		
-		int randPos = rand.nextInt(s+1);
+		//random starting positions of prey and predator
+		Point prey = new Point(rand.nextInt(n)+1, rand.nextInt(n)+1); //we want the interval [1;n]
+		Point predator = new Point(rand.nextInt(n)+1, rand.nextInt(n)+1);
+		int mover = newMover(rand, s); //generates a new value to add to the beasts
+		
+		printPositions(prey, predator); //prints the initial positions of the beasts
+		
+		for (int i = 1; i <= t; i++) {
+			moveBeast(prey, predator, mover, i, n); //since prey moves first, then modulo 2 can itself switch between beasts to move
+			mover = newMover(rand, s);
+			
+			printPositions(prey, predator); //task specifies that the positions of both beasts are printed "after each move"
+			
+			checkCollision(prey, predator);
+		}
+	}
+	
+	public static int newMover(Random rand, int s) {
+		int randPos = rand.nextInt(s+1); //random movement value in the interval [0;s]
 		int randBool = rand.nextBoolean() ? 1 : 0; //gives 1, if true and 0 if not
 		int randMove = randPos - 2 * randPos * randBool; //finally a pseudo-random value between [-s;s]
 		
-		
+		return randMove;
 	}
-
+	
+	public static void moveBeast(Point prey, Point predator, int mover, int turn, int gridSize) {
+		if (turn % 2 == 1) { //checks whether to move the prey or the predator
+			
+			if (prey.x + mover <= gridSize) {
+				prey.x += mover;
+			} else {
+				prey.x = gridSize;
+			}
+			
+			if (prey.y + mover <= gridSize) {
+				prey.y += mover;
+			} else {
+				prey.y = gridSize;
+			}
+			
+		}
+		else {
+			//the task seems to specify that the mover is added to the coords, and not that the distance moved has to max be 'mover'
+		}
+	}
+	
+	public static void printPositions(Point prey, Point predator) {
+		System.out.println("[" + prey.x + ";" + prey.y + "] [" + predator.x + ";" + predator.y + "]");
+	}
+	
+	public static void checkCollision(Point prey, Point predator) {
+		if (prey.x == predator.x && prey.y == predator.y) {
+			System.out.println("Caught!");
+			System.exit(0);
+		}
+	}
+	
 }
